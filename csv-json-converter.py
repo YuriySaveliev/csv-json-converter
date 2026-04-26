ba2d4ff5cb3533a202ad7fde25e281290ece9b43
Yuriy Savelyev <savjurnick@gmail.com>
2026-04-26 17:30:30 +0300

    Improve logging

--- csv-json-converter.py
+++ csv-json-converter.py
@@ -2,49 +2,56 @@ import csv
 import random
 import math
 import json
+import logging
 
 COLUMNS_COUNT = 5
 ROWS_COUNT = 50
 
-columns = range(COLUMNS_COUNT)
-rows = range(ROWS_COUNT)
+columns = list(range(COLUMNS_COUNT))
+rows = list(range(ROWS_COUNT))
 DATA_FILE_NAME = 'rnd.csv'
 OUTPUT_FILE_NAME = 'output.json'
 
 
-def create_data(row_number, column_number):
-    return [[math.ceil(random.random() * 100) for column in column_number] for row in row_number]
+def create_data(rows: list, columns: list) -> list:
+    return [[random.randint(1, 100) for _ in columns] for _ in rows]
 
 
-def write_csv_data(file_name, input_data):
+def write_csv_data(file_name: str, input_data: list) -> None:
     with open(file_name, 'w') as csvfile:
         writer = csv.writer(csvfile)
         for row in input_data:
             writer.writerow(row)
 
 
-def create_json_data(file_name):
+def create_json_data(file_name: str) -> dict:
     json_data = {}
 
     with open(file_name, 'r') as csvfile:
         reader = csv.reader(csvfile)
         for index, row in enumerate(reader):
-            new_list = [int(item) for item in row]
-            json_data.update(dict({index: new_list}))
+            item = [int(item) for item in row]
+            json_data.update({index: item})
 
     return json_data
 
 
-def create_output_file(file_name, data):
+def create_output_file(file_name: str, data: dict) -> None:
     with open(file_name, 'w') as jsonfile:
         json.dump(data, jsonfile)
 
 
-def init():
+def main():
+    logging.basicConfig(level=logging.INFO)
+    logging.info('Starting app...')
+
     csv_content = create_data(rows, columns)
     write_csv_data(DATA_FILE_NAME, csv_content)
     json_content = create_json_data(DATA_FILE_NAME)
     create_output_file(OUTPUT_FILE_NAME, json_content)
 
+    logging.info('Done')
 
-init()
+
+if __name__ == '__main__':
+    main()
